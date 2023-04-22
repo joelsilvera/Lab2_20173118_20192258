76993976a8d001eb69ab18e560a924c6f5d0aac3
joelsilvera <joel.silvera@pucp.edu.pe>
2023-04-22 12:26:13 -0500

    nuevo proyectito

---
+++ app/src/main/java/com/example/lab2_20173118_20192258/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.lab2_20173118_20192258;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //Este es un comentarioblablabalb.kdfnmblarkgmvSDLKjbz
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
