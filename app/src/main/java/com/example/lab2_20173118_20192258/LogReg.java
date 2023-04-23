dc0ca19878f717a4329a512d824cebbc93df3be4
angelo <a20192258@pucp.edu.pe>
2023-04-22 23:10:30 -0500

    Activity para Login y Registro

---
+++ app/src/main/java/com/example/lab2_20173118_20192258/LogReg.java
@@ -0,0 +1,14 @@
+package com.example.lab2_20173118_20192258;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LogReg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_reg);
+    }
+}
