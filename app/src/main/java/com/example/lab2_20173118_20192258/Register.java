ea9c8f7ee6c4237a86271a954f9864ee089d9916
angelo <a20192258@pucp.edu.pe>
2023-04-23 09:08:49 -0500

    Ultimos cambios

--- app/src/main/java/com/example/lab2_20173118_20192258/Register.java
+++ app/src/main/java/com/example/lab2_20173118_20192258/Register.java
@@ -3,15 +3,20 @@ package com.example.lab2_20173118_20192258;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.Spannable;
+import android.text.SpannableString;
 import android.text.TextWatcher;
+import android.text.style.StyleSpan;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
 
@@ -147,6 +152,33 @@ public class Register extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        TextView myTextView = findViewById(R.id.textView2);
+        String text = "He leído y acepto los ";
+        String boldText = "Término y Condiciones";
+
+        // Create a SpannableString and set the bold style to the "bold" part of the string
+        SpannableString spannableString = new SpannableString(text + boldText);
+        StyleSpan boldStyle = new StyleSpan(Typeface.BOLD);
+        spannableString.setSpan(boldStyle, text.length(), text.length() + boldText.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        // Set the SpannableString as the text of the TextView
+        myTextView.setText(spannableString);
+
+
+        TextView myTextView2 = findViewById(R.id.textView3);
+        String text2 = "¿Usted ha leído y acepta los términos y condiciones para el tratamiento de sus datos personales contenidos en la ";
+        String boldText2 = "Política de Privacidad Web";
+        String normalText2 = " ?";
+
+        // Create a SpannableString and set the bold style to the "bold" part of the string
+        SpannableString spannableString2 = new SpannableString(text2 + boldText2 + normalText2);
+        StyleSpan boldStyle2 = new StyleSpan(Typeface.BOLD);
+        spannableString2.setSpan(boldStyle2, text2.length(), text2.length() + boldText2.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        // Set the SpannableString as the text of the TextView
+        myTextView2.setText(spannableString2);
+
 
     }
 }
