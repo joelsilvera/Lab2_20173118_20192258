c719245c28493abf4147676d7a3ef977cd3183f6
angelo <a20192258@pucp.edu.pe>
2023-04-23 10:32:11 -0500

    Botones de la parte superior v2

--- app/src/main/java/com/example/lab2_20173118_20192258/HomePage.java
+++ app/src/main/java/com/example/lab2_20173118_20192258/HomePage.java
@@ -3,6 +3,9 @@ package com.example.lab2_20173118_20192258;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class HomePage extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        ImageButton button = findViewById(R.id.imageButton8);
+        button.setOnClickListener( view -> {
+            Toast.makeText(HomePage.this,"Hola Mundo", Toast.LENGTH_SHORT).show(); //Mensajito rápido en la parte inferior
+        });
     }
 }
