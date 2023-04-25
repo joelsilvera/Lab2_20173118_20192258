07c1415870e0fb80ae54669df4d6152ae96d5532
joelsilvera <joel.silvera@pucp.edu.pe>
2023-04-25 00:00:27 -0500

    color de barraverde

--- app/src/main/java/com/example/lab2_20173118_20192258/ejercicio3.java
+++ app/src/main/java/com/example/lab2_20173118_20192258/ejercicio3.java
@@ -3,14 +3,25 @@ package com.example.lab2_20173118_20192258;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+
+
 
 public class ejercicio3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Window window = getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(Color.parseColor("#8ED64F"));
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ejercicio3);
 
